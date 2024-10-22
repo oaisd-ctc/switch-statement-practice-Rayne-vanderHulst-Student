7376e1ea16563fa8d1dd2a60b3959311a6177a59
Rayne <rava438@student.oaisd.org>
2024-10-22 09:50:01 -0400

    Switch Statements
    Switch Statements

---
+++ Switch Statements/Program (1).cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class program
+{
+
+public static void Main(string[] args)
+{
+
+Console.WriteLine("give a number 0-10");
+int x = int.Parse(Console.ReadLine());
+switch(x)
+{
+    case 0:
+    Console.WriteLine("your number is 0");
+    break;
+
+    case 1:
+    Console.WriteLine("your number is 1");
+    break;
+
+    case 2:
+    Console.WriteLine("your number is 2");
+    break;
+
+    case 3:
+    Console.WriteLine("your number is 3");
+    break;
+
+    case 4:
+    Console.WriteLine("your number is 4");
+    break;
+
+    case 5:
+    Console.WriteLine("your number is 5");
+    break;
+
+    case 6:
+    Console.WriteLine("your number is 6");
+    break;
+
+    case 7:
+    Console.WriteLine("your number is 7");
+    break;
+
+    case 8:
+    Console.WriteLine("your number is 8");
+    break;
+
+    case 9:
+    Console.WriteLine("your number is 9");
+    break;
+
+    case 10:
+    Console.WriteLine("your number is 10");
+    break;
+
+
+}
+
+}
+}
